67eafa07ee55741c32452e8081d86472d87a747e
nayoon <nayoonk928@gmail.com>
2022-09-09 20:51:56 +0900

    loop statements (while, do while, for)

--- app/src/main/java/ny/tutorials/kotlinbasics/Basics.kt
+++ app/src/main/java/ny/tutorials/kotlinbasics/Basics.kt
@@ -12,103 +12,105 @@ fun main() {
     // immutable variable = val, mutable variable = var
     // type string
     //type inference finds out the type from context
+/*
     val myName = "Nayoon"
-    //print("Hello " +myName)
+    print("Hello " +myName)
+*/
 
     // https://kotlinlang.org/docs/basic-types.html
     // Integer Types: Byte (8 bit), Short (16 bit), Int (32 bit), Long (64 bit)
-    val myByte: Byte = 13
+/*    val myByte: Byte = 13
     val myShort: Short = 125
     val myInt: Int = 123123123
-    val myLong: Long = 39_812_309_487_120_300
+    val myLong: Long = 39_812_309_487_120_300*/
 
     // Floating Point number Types: Float (32 bit), Double (64 bit)
-    val myFloat: Float = 13.37F
-    val myDouble: Double = 3.14159265358979323846
+/*    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.14159265358979323846*/
 
     // Booleans he type Boolean is used to represent logical values.
     // It can have two possible values true and false.
-    var isSunny: Boolean = true
-    isSunny = false
+/*    var isSunny: Boolean = true
+    isSunny = false*/
 
     // Characters
-    val letterChar = 'A'
-    val digitChar = '1'
+/*    val letterChar = 'A'
+    val digitChar = '1'*/
 
     // Strings
-    val myStr = "Hello World"
+   /* val myStr = "Hello World"
     var firstCharInStr = myStr[0]
     var lastCharInStr = myStr[myStr.length - 1]
     var myLength = myStr.length
-    //println("First character " +firstCharInStr)
-    //println("Last character $lastCharInStr")
-    //println("First character $firstCharInStr and the length of myStr is $myLength")
+    println("First character " +firstCharInStr)
+    println("Last character $lastCharInStr")
+    println("First character $firstCharInStr and the length of myStr is $myLength")*/
 
     // https://www.callicoder.com/kotlin-operators/
     // Arithmetic operators (+, -, *, /, %)
-    var result = 5 + 3
+   /* var result = 5 + 3
     val a = 5.0
     val b = 3
     var resultDouble: Double
     resultDouble = a / b
-    //print(resultDouble)
+    print(resultDouble)*/
 
     // Comparison operators (==, !=, <, >, <=, >=)
-    val isEqual = 5 == 3
-    //println("isEqual is $isEqual")
+    /*val isEqual = 5 == 3
+    println("isEqual is $isEqual")
 
     val isNotEqual = 5 != 5
     // String interpolation
-    //println("isNotEqual is $isNotEqual")
-    //println("is-5less3 ${-5 < 3}")
-    //println("is5LowerEqual3 ${5 <= 3}")
-    //println("is5GreaterEqual3 ${5 >= 3}")
+    println("isNotEqual is $isNotEqual")
+    println("is-5less3 ${-5 < 3}")
+    println("is5LowerEqual3 ${5 <= 3}")
+    println("is5GreaterEqual3 ${5 >= 3}")*/
 
     // Assignment operators (+=, -=, *=, /=, %=)
-    var myNum = 5
+/*    var myNum = 5
     myNum += 3
-    //println("myNum is $myNum")
+    println("myNum is $myNum")*/
 
     // Increment & Decrement operators (++, --)
-    myNum++
-    //println("myNum is $myNum")
-    // increments after use
-    //println("myNum is ${myNum++}")
-    // increments before use
-    //println("myNum is ${++myNum}")
+/*    myNum++
+    println("myNum is $myNum")
+     //increments after use
+    println("myNum is ${myNum++}")
+     //increments before use
+    println("myNum is ${++myNum}")*/
 
     // if statement
     // https://kotlinlang.org/docs/reference/control-flow.html
     // https://kotlinlang.org/docs/control-flow.html
-    var heightPerson1 = 170
+    /*var heightPerson1 = 170
     var heightPerson2 = 170
 
     if (heightPerson1 > heightPerson2) {
-        //println("use raw force")
+        println("use raw force")
     } else  if (heightPerson1 == heightPerson2) {
-        //println("use your power technique 1337")
+        println("use your power technique 1337")
     } else {
-        //println("use technique")
+        println("use technique")
     }
 
     val age = 21
     if (age >= 21) {
-        //println("you may drink in the US")
+        println("you may drink in the US")
     } else if (age >= 18) {
-        //println("you may vote now")
+        println("you may vote now")
     } else if (age >= 16){
-        //println("you may drive now")
+        println("you may drive now")
     } else {
-        //println("you are too young")
+        println("you are too young")
     }
 
     var name = "Denis"
     if (name == "Denis") {
-        //println("Welcome home denis")
+        println("Welcome home denis")
     } else {
-        //println("Who are you?")
+        println("Who are you?")
     }
-
+*/
     /*var isRainy = true
     if(isRainy)
        println("It's rainy")*/
@@ -146,41 +148,41 @@ fun main() {
 
     // when statement
     // https://kotlinlang.org/docs/control-flow.html
-    var season = 3
+    /*var season = 3
     when(season){
-        /*1 -> println("Spring")
+        1 -> println("Spring")
         2 -> println("Summer")
         3 -> {
             println("Fall")
             println("Autumn")
         }
         4 -> println("Winter")
-        else -> println("Invalid Season")*/
+        else -> println("Invalid Season")
     }
 
     var month = 3
     when(month){
-        /*in 3..5 -> println("Spring")
+        in 3..5 -> println("Spring")
         in 6..8 -> println("Summer")
         in 9..11 -> println("Fall")
         12, 1, 2 -> println("Winter")
-        else -> println("Invalid Season")*/
+        else -> println("Invalid Season")
     }
 
     when(age){
-//        !in 0..20 -> println("you may drink in the US")
-//        in 18..20 -> println("you may vote now")
-//        16, 17 -> println("you may drive now")
-//        else -> println("you are too young")
+        !in 0..20 -> println("you may drink in the US")
+        in 18..20 -> println("you may vote now")
+        16, 17 -> println("you may drive now")
+        else -> println("you are too young")
     }
 
     var x : Any = "13.37f"
     when(x){
-//        is Int -> println("$x is an Int")
-//        is Double -> println("$x is a Double")
-//        is String -> println("$x is a String")
-//        else -> println("$x is none of the above")
-    }
+        is Int -> println("$x is an Int")
+        is Double -> println("$x is a Double")
+        is String -> println("$x is a String")
+        else -> println("$x is none of the above")
+    }*/
 
     // 식으로 된 When statement
 
@@ -205,4 +207,45 @@ fun main() {
     }
 //then print x with the result
     print("$x $result")*/
+
+    // While loop executes a block of code repeatedly as long as a given condition is true
+    // https://kotlinlang.org/docs/control-flow.html
+    /*var x = 1
+    while(x <= 10) {
+        print("$x")
+        x++
+    }
+    println("\nwhile loop is done")*/
+
+    // do while loop
+    /*var x = 15
+    do {
+        print("$x")
+        x++
+    } while (x <= 10)
+    println("\ndo while loop is done")*/
+
+    // while loop with if statement
+    /*var feltTemp = "cold"
+    var roomTemp = 10
+    while (feltTemp == "cold"){
+        roomTemp++
+        if(roomTemp >= 20){
+            feltTemp = "comfy"
+            println("It's comfy now")
+        }
+    }*/
+
+    // for loop
+    /*for(num in 1..10){
+        print("$num")
+    }
+
+    for(i in 1 until 10){ // Samd as - for(i in 1.until(10))
+        print("$i ")
+    }
+    println("__________")
+    for(i in 10 downTo 1 step 2){ // Same as - for(i in 10.downTo(2).step(2))
+        print("$i ")
+    }*/
 }
--- app/src/main/java/ny/tutorials/kotlinbasics/Exercises.kt
+++ app/src/main/java/ny/tutorials/kotlinbasics/Exercises.kt
@@ -15,6 +15,7 @@ fun main() {
  */
 
 // if statement 연습 문제
+/*
 fun main() {
     var age = 15
     if (age >= 21) {
@@ -26,4 +27,34 @@ fun main() {
     } else {
         println("you are too young")
     }
-}
+}*/
+
+// while loop 연습
+/*fun main() {
+    var x = 100
+    while (x >= 0) {
+        println("$x")
+        x -= 2
+    }
+    println("while loop is done")
+}*/
+
+// loop statement 연습 문제
+/*
+fun main() {
+    for(num in 1..10000){
+        if(num == 9001)
+            println("IT'S OVER 9000!!!")
+    }
+
+    var humidity = "humid"
+    var humidityLevel = 80
+    while(humidity == "humid"){
+        humidityLevel -= 5
+        println("humidity decreased")
+        if(humidityLevel <= 60){
+            println("it's comfy now")
+            humidity = "comfy"
+        }
+    }
+}*/
